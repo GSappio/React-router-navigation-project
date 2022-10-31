e7770eef6db6d6d294be7f4023d281c2772d50e2
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-10-31 12:44:54 -0300

    Creating the project folder structure

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = props => (
+
+    <aside className="About">
+        <h1>Sobre</h1>
+        <p>O nosso sitema foi  criado...</p>
+    </aside>
+
+)
+
+export default About
